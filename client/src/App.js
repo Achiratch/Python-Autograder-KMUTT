cf7b8582f3a35b9cdd36aaf84d6f309267d8b27e
achiratch <achiratch.cmp@gmail.com>
2021-02-05 17:44:01 +0700

    :pouting_woman::purple_heart: [Redux] Create Stores and Add Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import LandingPage  from './component/landing_page'
-import RegisterPage  from './component/register_page'
-import  HomePage  from './component/home_page'
-import { BrowserRouter , Router, Route, Switch } from 'react-router-dom';
-import {ProtectedRoute} from './component/auth/protected_route'
-import './App.css'
+import { Provider } from "react-redux";
+import store from './redux/store'
+
+import LandingPage from "./component/landing_page";
+import RegisterPage from "./component/register_page";
+import HomePage from "./component/home_page";
+import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
+import { ProtectedRoute } from "./component/auth/protected_route";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path= "/" component={LandingPage}/>
-        <Route exact path= "/register" component={RegisterPage}/>
-        <ProtectedRoute exact path= "/home" component={HomePage}/>
-        <Route path= "*" component={() => "404 NOT FOUND"} />
-      </Switch>
+      <Provider store={store}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <ProtectedRoute exact path="/home" component={HomePage} />
+          <Route path="*" component={() => "404 NOT FOUND"} />
+        </Switch>
+      </Provider>
     </div>
   );
 }
