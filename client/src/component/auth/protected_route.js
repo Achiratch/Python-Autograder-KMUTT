5a1b927d8d3cf918beea7c598c7c7833fe08a109
Inam <chanayut.ko@gmail.com>
2021-02-13 23:32:29 +0700

    :sparkles:[Front-end]: Add role checking to ProtectedRoute

--- client/src/component/auth/protected_route.js
+++ client/src/component/auth/protected_route.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import {Redirect, Route} from 'react-router-dom'
-import auth from './auth'
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import auth from "./auth";
+import { useSelector } from "react-redux";
 
-export const ProtectedRoute = ({component: Component, ...rest}) => {
-    return(
-        <Route 
-            {...rest} 
-             render={
-            (props) => {
-                if(auth.isAuthenticated()){
-                    return <Component {...props} />;
-                }
-                else {
-                    return (
-                      <Redirect
-                        to={{
-                          pathname: "/",
-                          state: {
-                            from: props.location
-                          }
-                        }}
-                      />
-                    );
-                  } 
-            }
-        }/>
-    )
-}
+export const ProtectedRoute = ({ component: Component, role, ...rest }) => {
+  const roleChecked = useSelector((state) => state.auth.user.role);
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (auth.isAuthenticated()) {
+          if (role !== roleChecked) {
+            return (
+              <Redirect
+                to={{
+                  pathname: "/403",
+                  state: {
+                    from: props.location,
+                  },
+                }}
+              />
+            );
+          } else {
+            return <Component {...props} />;
+          }
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
