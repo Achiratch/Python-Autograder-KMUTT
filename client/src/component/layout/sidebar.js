53718ac337ae6904723645824b34699fd9588f58
achiratch <achiratch.cmp@gmail.com>
2021-02-28 13:19:22 +0700

    :nail_care: [Front-end] Add activeClassName in Sidebar

--- client/src/component/layout/sidebar.js
+++ client/src/component/layout/sidebar.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./layout.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faUser } from "@fortawesome/free-regular-svg-icons";
 import { faBook } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 
 function Sidebar() {
-  const [isActive, setActive] = useState(false);
-
-  const activeClass = () => {
-    setActive(!isActive);
-  };
   return (
     <div className="sidebar">
       <div className="sidebar-head">
@@ -19,32 +13,39 @@ function Sidebar() {
         <h2>Introduction coding Python</h2>
       </div>
       <div className="sidebar-link">
-        <Link
+        <NavLink
+          exact
           to="/exercises"
-          className="menu-link active-sidebar"
-          onClick={activeClass}
+          activeClassName="active-sidebar"
+          className="menu-link "
         >
           <span>
             <FontAwesomeIcon icon={faBook} size="lg" />
           </span>
           <span>Exercise</span>
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
+          exact
           to="scorebook"
-          className="menu-link active-sidebar"
-          onClick={activeClass}
+          className="menu-link"
+          activeClassName="active-sidebar"
         >
           <span>
             <FontAwesomeIcon icon={faStar} size="lg" />
           </span>
           <span>Score Book</span>
-        </Link>
-        <Link to="/member" className="menu-link active-sidebar" onClick={activeClass}>
+        </NavLink>
+        <NavLink
+          exact
+          to="/member"
+          className="menu-link "
+          activeClassName="active-sidebar"
+        >
           <span>
             <FontAwesomeIcon icon={faUser} size="lg" />
           </span>
           <span>Member</span>
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
