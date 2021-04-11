7e9ac5ec07fe245896cda7104e3730d6b3e47a9c
Achiratch <48543080+Achiratch@users.noreply.github.com>
2021-04-11 22:57:34 +0700

    :bulb: [Front-end] Add exercises component

--- client/src/component/pages/exercises_page/exercises_page.js
+++ client/src/component/pages/exercises_page/exercises_page.js
@@ -3,8 +3,18 @@ import Footer from "../../layout/footer";
 import Navbar from "../../layout/navbar";
 import Sidebar from "../../layout/sidebar";
 
+//CSS
+import "./exercises_page.css";
+
 //ANTD
 import { Breadcrumb } from "antd";
+
+//Material-UI
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 
 //Redux
 import { connect } from "react-redux";
@@ -34,6 +44,112 @@ class ExercisesPage extends Component {
                 </Breadcrumb.Item>
                 <Breadcrumb.Item>Exercise</Breadcrumb.Item>
               </Breadcrumb>
+            </div>
+            <div className="button-exercises">
+              <button className="add-exercises-button">
+                <span className="icon-button">
+                  <AddBoxIcon />
+                </span>
+                Add Exercise
+              </button>
+              <button className="delete-exercises-button">
+                <span className="icon-button">
+                  <DeleteIcon />
+                </span>
+                Delete Exercise
+              </button>
+            </div>
+            <div className="flex execercise-box">
+              <div className="exercise-name">
+                <h1>
+                  <span id="light" className="icon-button">
+                    <EmojiObjectsIcon style={{ fontSize: 25 }} />
+                  </span>
+                  Exercise : Python Calculator
+                </h1>
+                <h2>By Bill Gate</h2>
+              </div>
+              <div className="exercise-name">
+                <h1>Due Date</h1>
+                <h2>25/8/2020 at 12.00 PM</h2>
+              </div>
+              <div className="exercise-name ">
+                <button className="stat-button">
+                  <span className="icon-button">
+                    <EqualizerIcon />
+                  </span>
+                  Stat
+                </button>
+              </div>
+            </div>
+            <div className="flex execercise-box">
+              <div className="exercise-name">
+                <h1>
+                  <span id="light" className="icon-button">
+                    <EmojiObjectsIcon style={{ fontSize: 25 }} />
+                  </span>
+                  Exercise : Python Calculator
+                </h1>
+                <h2>By Bill Gate</h2>
+              </div>
+              <div className="exercise-name">
+                <h1>Due Date</h1>
+                <h2>25/8/2020 at 12.00 PM</h2>
+              </div>
+              <div className="exercise-name ">
+                <button className="stat-button">
+                  <span className="icon-button">
+                    <EqualizerIcon />
+                  </span>
+                  Stat
+                </button>
+              </div>
+            </div>
+            <div className="flex execercise-box">
+              <div className="exercise-name">
+                <h1>
+                  <span id="light" className="icon-button">
+                    <EmojiObjectsIcon style={{ fontSize: 25 }} />
+                  </span>
+                  Exercise : Python Calculator
+                </h1>
+                <h2>By Bill Gate</h2>
+              </div>
+              <div className="exercise-name">
+                <h1>Due Date</h1>
+                <h2>25/8/2020 at 12.00 PM</h2>
+              </div>
+              <div className="exercise-name ">
+                <button className="stat-button">
+                  <span className="icon-button">
+                    <EqualizerIcon />
+                  </span>
+                  Stat
+                </button>
+              </div>
+            </div>
+            <div className="flex execercise-box">
+              <div className="exercise-name">
+                <h1>
+                  <span id="light" className="icon-button">
+                    <EmojiObjectsIcon style={{ fontSize: 25 }} />
+                  </span>
+                  Exercise : Python Calculator
+                </h1>
+                <h2>By Bill Gate</h2>
+              </div>
+              <div className="exercise-name">
+                <h1>Due Date</h1>
+                <h2>25/8/2020 at 12.00 PM</h2>
+              </div>
+              <div className="exercise-name ">
+                <button className="stat-button">
+                  <span className="icon-button">
+                    <EqualizerIcon />
+                  </span>
+                  Stat
+                </button>
+              </div>
             </div>
           </div>
         </div>
