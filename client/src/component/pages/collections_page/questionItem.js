3dc25f80163a0db1298c34c6539a34d94668e9c9
Achiratch <48543080+Achiratch@users.noreply.github.com>
2021-04-23 14:58:14 +0700

    ✍️  [Front-end] Edit QuestionBox

--- client/src/component/pages/collections_page/questionItem.js
+++ client/src/component/pages/collections_page/questionItem.js
@@ -3,25 +3,27 @@ import React, { Component } from "react";
 import EqualizerIcon from "@material-ui/icons/Equalizer";
 import PersonIcon from "@material-ui/icons/Person";
 import AssignmentIcon from "@material-ui/icons/Assignment";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
+//ANTD
 import { Popover } from "antd";
-
-
 
 class QuestionItem extends Component {
   render() {
     const { question } = this.props;
     const content = (
-        <div>
-          <p>Description: {question.description}</p>
-        </div>
-      );
+      <div>
+        <p>Description: {question.description}</p>
+      </div>
+    );
     return (
       <div>
-        <Popover content={content}  trigger="hover">
+        <Popover content={content} trigger="hover">
           <div className="flex question-box">
-            <div className="exercise-name">
-              <h1>
+            <div className="name-group">
+              <h1 className="name">
                 <span className="icon-button">
                   <AssignmentIcon style={{ fontSize: 25 }} />
                 </span>
@@ -29,18 +31,28 @@ class QuestionItem extends Component {
               </h1>
             </div>
 
-            <div className="exercise-name">
-              <h2>
+            <div className="createby-group">
+              <h2 className="createby">
                 <span className="icon-button">
                   <PersonIcon />
                 </span>
                 Bill Gate
               </h2>
             </div>
-            <div className="exercise-name">
-              <h1 id={"level-" + question.level}>Level {question.level}</h1>
+            <div className="level-group">
+              <h1 id={"level-" + question.level} className="level">
+                Level {question.level}
+              </h1>
             </div>
-            <div className="exercise-name ">
+            <div className="button-group">
+              <IconButton aria-label="edit" size="medium">
+                <EditIcon />
+              </IconButton>
+              <IconButton aria-label="delete" color="secondary" size="medium">
+                <DeleteIcon fontSize="inherit" />
+              </IconButton>
+            </div>
+            <div className="button-train">
               <button className="question-button">
                 <span className="icon-button">
                   <EqualizerIcon />
@@ -52,8 +64,8 @@ class QuestionItem extends Component {
         </Popover>
 
         <div className="flex question-box">
-          <div className="exercise-name">
-            <h1>
+          <div className="name-group">
+            <h1 className="name">
               <span className="icon-button">
                 <AssignmentIcon style={{ fontSize: 25 }} />
               </span>
@@ -61,18 +73,26 @@ class QuestionItem extends Component {
             </h1>
           </div>
 
-          <div className="exercise-name">
-            <h2>
+          <div className="createby-group">
+            <h2 className="createby">
               <span className="icon-button">
                 <PersonIcon />
               </span>
-              Bill Gate
+              Inam Wannafly
             </h2>
           </div>
-          <div className="exercise-name">
-            <h1>Level 5</h1>
+          <div className="level-group">
+            <h1 className="level">Level 5</h1>
           </div>
-          <div className="exercise-name ">
+          <div className="button-group">
+            <IconButton aria-label="edit" size="medium">
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete" color="secondary" size="medium">
+              <DeleteIcon fontSize="inherit" />
+            </IconButton>
+          </div>
+          <div className="button-train">
             <button className="question-button">
               <span className="icon-button">
                 <EqualizerIcon />
