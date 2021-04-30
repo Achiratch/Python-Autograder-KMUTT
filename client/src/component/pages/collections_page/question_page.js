5f08da54dff9346ff4b3daf041d57aa034dd5bd7
Achiratch <48543080+Achiratch@users.noreply.github.com>
2021-04-30 17:45:03 +0700

    💚  [Front-end] Create funtion import Datacamp light

--- client/src/component/pages/collections_page/question_page.js
+++ client/src/component/pages/collections_page/question_page.js
@@ -11,94 +11,113 @@ import { faMedal } from "@fortawesome/free-solid-svg-icons";
 //ANTD
 import { Input } from "antd";
 
+import LinearProgress from "@material-ui/core/LinearProgress";
+
 //Redux
 import { connect } from "react-redux";
 import { getQuestion } from "../../../redux/actions/collectionAction";
+
+function loadScript(url) {
+  // Adding the script tag to the head as suggested before
+  var head = document.head;
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+
+  // Fire the loading
+  head.appendChild(script);
+}
+
 class QuestionPage extends Component {
   componentDidMount() {
+    //loadScript("https://cdn.datacamp.com/dcl-react.js.gz");
     console.log(this.props.match.params.id);
     this.props.getQuestion(this.props.match.params.id);
   }
   render() {
     const { TextArea } = Input;
-    const { collection } = this.props;
+    const { collection,sct } = this.props;
     console.log(collection.question.name);
-    return (
-      <div>
-        <Navbar />
-        <div className="body">
-          <div className="page-content">
-            <div className="header flex">
-              <div className="name-header">
-                <h1>Python Calculate</h1>
-              </div>
-              <div className="description-header">
-                <span id="description">
-                  <b>Description</b>
-                </span>
-                <TextArea
-                  defaultValue="In the Python script on the right, you can type
-                  Python code to solve the exercises. If you hit Run Code or
-                  Submit Answer, your python script (script.py) is executed and
-                  the output is shown in the IPython Shell. Submit Answer checks
-                  whether your submission is correct and gives you feedback. You
-                  can hit Run Code and Submit Answer as often as you want. If
-                  you're stuck, you can click Get Hint, and ultimately Get
-                  Solution. You can also use the IPython Shell interactively by
-                  simply typing commands and hitting Enter. When you work in the
-                  shell directly, your code will not be checked for correctness
-                  so it is a great way to experiment."
-                  disabled={false}
-                  autoSize={{ minRows: 3, maxRows: 4 }}
-                  id="outputText"
-                  cols="200"
-                  bordered={false}
-                  readOnly
-                ></TextArea>
-              </div>
-              <div
-                id={`background-level-${collection.question.level}`}
-                className="level-header flex"
-              >
-                <span>
-                  <FontAwesomeIcon
-                    className="fa-medal"
-                    icon={faMedal}
-                    color="white"
-                  />
-                </span>
-                <h2>Level {collection.question.level}</h2>
-              </div>
-            </div>
-            <div className="edit-code">
-              <div
-                data-datacamp-exercise
-                data-lang="python"
-                data-height="500rem"
-              >
-                <code data-type="pre-exercise-code">
-                  # This will get executed each time the exercise gets
-                  initialized b = 6
-                </code>
-                <code data-type="sample-code">
-                  # Create a variable a, equal to 5 # Print out a
-                </code>
-                <code data-type="solution"></code>
-                <code data-type="sct">
-                  test_object("a") test_function("print") success_msg("Great
-                  job!")
-                </code>
+    console.log(collection.question.level);
+    console.log(sct);
 
-                <div data-type="hint">
-                  Use the assignment operator (<code>=</code>) to create the
-                  variable
-                  <code>a</code>.
-                </div>
+    let dataCamp;
+    if (collection.loading === true) {
+      dataCamp = <div className="page-content"><LinearProgress /></div>;
+    } else
+    loadScript("https://cdn.datacamp.com/dcl-react.js.gz");
+      dataCamp = (
+        <div className="page-content">
+          <div className="header flex">
+            <div className="name-header">
+              <h1>Python Calculate</h1>
+            </div>
+            <div className="description-header">
+              <span id="description">
+                <b>Description</b>
+              </span>
+              <TextArea
+                defaultValue="In the Python script on the right, you can type
+        Python code to solve the exercises. If you hit Run Code or
+        Submit Answer, your python script (script.py) is executed and
+        the output is shown in the IPython Shell. Submit Answer checks
+        whether your submission is correct and gives you feedback. You
+        can hit Run Code and Submit Answer as often as you want. If
+        you're stuck, you can click Get Hint, and ultimately Get
+        Solution. You can also use the IPython Shell interactively by
+        simply typing commands and hitting Enter. When you work in the
+        shell directly, your code will not be checked for correctness
+        so it is a great way to experiment."
+                disabled={false}
+                autoSize={{ minRows: 3, maxRows: 4 }}
+                id="outputText"
+                cols="200"
+                bordered={false}
+                readOnly
+              ></TextArea>
+            </div>
+            <div
+              id={`background-level-${collection.question.level}`}
+              className="level-header flex"
+            >
+              <span>
+                <FontAwesomeIcon
+                  className="fa-medal"
+                  icon={faMedal}
+                  color="white"
+                />
+              </span>
+              <h2>Level {collection.question.level}</h2>
+            </div>
+          </div>
+          <div className="edit-code">
+            <div data-datacamp-exercise data-lang="python" data-height="500rem">
+              <code data-type="pre-exercise-code">
+                {/* {collection.question.preExercise.code} */}
+              </code>
+              <code data-type="sample-code">
+                {/* {collection.question.sample.code} */}
+              </code>
+              <code data-type="solution">
+                {/* {collection.question.solution.code} */}
+              </code>
+              <code data-type="sct">
+                {/* {collection.question.sct.code} */}
+              </code>
+
+              <div data-type="hint">
+                Use the assignment operator (<code>=</code>) to create the
+                variable
+                <code>a</code>.
               </div>
             </div>
           </div>
         </div>
-
+      );
+    return (
+      <div>
+        <Navbar />
+        <div className="body">{dataCamp}</div>
         <Footer />
       </div>
     );
