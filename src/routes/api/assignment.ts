27fd12db012c8449e9291d2acdb962c73a3e51f4
Inam <chanayut.ko@gmail.com>
2021-06-03 15:50:16 +0700

    :sparkles:[Back-end(assignment)] Add get questions in assignment

--- src/routes/api/assignment.ts
+++ src/routes/api/assignment.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, Application, NextFunction } from "express";
 const router = express.Router();
-import { CreateAssignment, GetAllAssignment, GetAssignmentById, DeleteAssignmentById, UpdateAssignmentById, GetAssignmentByCourseId } from '../../controllers/assignment'
+import { CreateAssignment, GetAllAssignment, GetAssignmentById, DeleteAssignmentById, UpdateAssignmentById, GetAssignmentByCourseId, GetQuestionsByAssignmentId } from '../../controllers/assignment'
 import { Authorize } from '../../_helpers/authorize'
 import ROLE from '../../models/Role'
 import { protect } from '../../middleware/auth'
@@ -14,6 +14,7 @@ router.route("/create").post(protect, CreateAssignment)
 router.route("/").get(protect, GetAllAssignment)
 router.route("/course/:id").get(protect, GetAssignmentByCourseId)
 router.route("/:id").get(protect, GetAssignmentById)
+router.route("/:id/questions").get(protect, GetQuestionsByAssignmentId)
 router.route("/:id").delete(protect, DeleteAssignmentById)
 router.route("/:id/update").put(protect, upload, UpdateAssignmentById)
 
