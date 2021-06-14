0f43428b220c742d586f102f27c042c1a8502aae
Inam <chanayut.ko@gmail.com>
2021-06-14 19:25:30 +0700

    setup main.yml for heroku deploy 5

--- dist/config/keys.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongoURI: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kziub.mongodb.net/Python-Autograder-KMUTT?retryWrites=true&w=majority`,
-    secretOrKey: `${process.env.SECRET}`,
-};
--- dist/models/Role.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ROLE = {
-    ADMIN: 'admin',
-    ASSISTANT: 'assistant',
-    STUDENT: 'student'
-};
-exports.default = ROLE;
